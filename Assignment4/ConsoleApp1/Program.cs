2ee12c2c21ce95e4664f9a7c9def635ddfcf5774
Gawiiiii <136977336+Gawiiiii@users.noreply.github.com>
2025-03-06 20:43:30 +0800

    Commit Assignment 4

---
+++ Assignment4/ConsoleApp1/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+//链表节点
+public class Node<T>
+{
+    public Node<T> Next { get; set; }
+    public T Data { get; set; }
+    public Node(T t)
+    {
+        Next = null;
+        Data = t;
+    }
+}
+
+//泛型链表类
+public class GenericList<T>
+{
+    private Node<T> head;
+    private Node<T> tail;
+    public GenericList()
+    {
+        tail = head = null;
+    }
+    public Node<T> Head
+    {
+        get => head;
+    }
+    public void Add(T t)
+    {
+        Node<T> n=new Node<T>(t);   
+        if(tail==null)
+        {
+            head = tail = n;
+        }
+        else
+        {
+            tail.Next = n;
+            tail = n;
+        }
+    }
+
+    public void forEach(Action<T> action)
+    {
+        Node<T> current = head;
+        while(current != null)
+        {
+            action(current.Data);
+            current = current.Next;
+        }
+    }
+}
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        GenericList<int> intlist = new GenericList<int>();
+        Console.Write("创建GenericList<int>：");
+        for(int i = 0; i < 10; i++)
+        {
+            int cur = Random.Shared.Next(0,100001);
+            intlist.Add(cur);
+            Console.Write($"{cur}");
+            if(i<9)
+            {
+                Console.Write(", ");
+            }
+            else
+            {
+                Console.WriteLine();
+            }
+        }
+        //打印链表元素
+        intlist.forEach(item=>Console.Write($"{item} "));
+        Console.WriteLine();
+
+        //求最大值
+        int mx = int.MinValue;
+        intlist.forEach(item =>
+        {
+            if (item > mx) mx = item;
+        });
+        Console.WriteLine($"最大值：{mx}");
+
+        //求最小值
+        int mi=int.MaxValue;
+        intlist.forEach(item =>
+        {
+            if (item < mi) mi = item;
+        });
+        Console.WriteLine($"最小值：{mi}");
+
+        //求和
+        int sum = 0;
+        intlist.forEach(item => sum += item);
+        Console.WriteLine($"和：{sum}");
+
+        Console.Read();
+    }
+}
---
+++ Assignment4/ConsoleApp2/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Threading;
+
+public class Clock
+{
+    public event Action? Tick;
+    public event Action? Alarm;
+
+    private int alarmHour;
+    private int alarmMinute;
+    private int alarmSecond;
+
+    public Clock(int hour, int minute, int second)
+    {
+        this.alarmHour = hour;
+        this.alarmMinute = minute;
+        this.alarmSecond = second;
+    }
+
+    public void start()
+    {
+        while(true)
+        {
+            Tick?.Invoke();
+            DateTime now= DateTime.Now;
+            if(now.Hour == alarmHour && now.Minute == alarmMinute && now.Second == alarmSecond)
+            {
+                Alarm?.Invoke();
+            }
+            Thread.Sleep(1000);
+        }
+    }
+}
+
+public class Progrm
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("闹钟程序启动......");
+
+        DateTime now = DateTime.Now;
+        int alarmHour = now.Hour, alarmMinute = now.Minute, alarmSecond = now.Second;
+        if(alarmSecond + 30 >= 60)
+        {
+            alarmSecond = (alarmSecond + 30) % 60;
+            if(alarmMinute + 1 >= 60)
+            {
+                alarmMinute = (alarmMinute + 1) % 60;
+                alarmHour += 1;
+            }
+            else
+            {
+                alarmMinute += 1;
+            }
+        }
+        else
+        {
+            alarmSecond += 30;
+        }
+
+        Console.WriteLine($"当前时间：{now:HH:mm:ss}");
+        Console.WriteLine($"闹钟设定在：{alarmHour:D2}:{alarmMinute:D2}:{alarmSecond:D2}");
+        Clock clock=new Clock(alarmHour, alarmMinute, alarmSecond);
+        clock.Tick += () =>
+        {
+            Console.WriteLine($"滴答... 当前时间：{DateTime.Now:HH:mm:ss}");
+        };
+        clock.Alarm += () =>
+        {
+            Console.WriteLine("！！！闹钟响了！该起床啦！！！");
+        };
+        clock.start();
+    }
+}
